58c0f8c0fe6781a0ec7a535e3a66cdbb2beb8c5e
Anurag Ajwani <anurag.ajwani@gmail.com>
2020-05-13 19:28:49 +0100

    Initial commit

---
+++ UniversalFrameworkDemo/UniversalFrameworkDemo/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UniversalFrameworkDemo
+//
+//  Created by Anurag Ajwani on 13/05/2020.
+//  Copyright © 2020 Anurag Ajwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        // Present LoginViewController here
+    }
+}
+
